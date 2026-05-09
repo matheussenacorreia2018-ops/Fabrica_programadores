81390ee99fa250c83815a495070a49ed38253d97
Matheus Sena <matheus.senacorreia2018@gmail.com>
2026-05-09 11:36:53 -0300

    Add files via upload

---
+++ 03 - Python_entradas/Entradas5.py
@@ -0,0 +1,16 @@
+# Autor: Matheus Sena
+# Projeto: Entradas f-string
+
+valor1 = int(input('Digite o primero valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+# exibindo os resultados
+print(f'O resultado da soma é:  {soma}')
+print(f'O resultado da subtracao é:  {subtracao}')
+print(f'O resultado da multiplicacao é: {multiplicacao}')
+print(f'O resultado da divisao é: {divisao}')
---
+++ 03 - Python_entradas/Imc.py
@@ -0,0 +1,8 @@
+# Autor: Matheus Sena
+# Projeto: Variável (float)
+
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura'))
+
+imc = peso / (altura * altura)
+print(f'Seu imc é: {imc}')
