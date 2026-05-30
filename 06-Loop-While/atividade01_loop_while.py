79c7289d5230b61a9eb52015c1b40513f96a875d
Matheus Sena <matheus.senacorreia2018@gmail.com>
2026-05-30 11:45:17 -0300

    Add files via upload

---
+++ 06-Loop-While/atividade01_loop_while.py
@@ -0,0 +1,11 @@
+#Autor: Matheus Sena 
+#Projeto: Loop While
+
+numero = 2
+i = 1
+
+while i <= 10:
+    print(f"valor de i antes da interação {i}")
+    print(f"{numero} x {i} = {numero * i}")
+    i = i + 1
+    print(f"valor do i depois da iteração {i}")
